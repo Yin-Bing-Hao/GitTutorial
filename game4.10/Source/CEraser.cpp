038e00ef954fd4bc2cf25a93a4a14ad9003f94e3
羅柏凱 <t106590013@ntut.org.tw>
2019-03-22 21:34:54 +0800

    mouse control solider v0.03

--- game4.10/Source/CEraser.cpp
+++ game4.10/Source/CEraser.cpp
@@ -4,97 +4,196 @@
 #include <ddraw.h>
 #include "audio.h"
 #include "gamelib.h"
+#include <vector>
 #include "CEraser.h"
+#include<iostream>
 
-namespace game_framework {
-	/////////////////////////////////////////////////////////////////////////////
-	// CEraser: Eraser class
-	/////////////////////////////////////////////////////////////////////////////
+namespace game_framework
+{
+/////////////////////////////////////////////////////////////////////////////
+// CEraser: Eraser class
+/////////////////////////////////////////////////////////////////////////////
 
-	CEraser::CEraser()
+CEraser::CEraser()
+{
+    Initialize();
+}
+
+int CEraser::GetX1()
+{
+    return x;
+}
+
+int CEraser::GetY1()
+{
+    return y;
+}
+
+int CEraser::GetX2()
+{
+    return x + animation.Width();
+}
+
+int CEraser::GetY2()
+{
+    return y + animation.Height();
+}
+
+void CEraser::Initialize()
+{
+    const int X_POS = 20;
+    const int Y_POS = 20;
+    x = X_POS;
+    y = Y_POS;
+    index_x = index_y = 1;
+    isMovingLeft = isMovingRight = isMovingUp = isMovingDown = false;
+}
+
+void CEraser::LoadBitmap()
+{
+	animation2.AddBitmap(IDB_PEOPLE2, RGB(255, 255, 255));
+    animation.AddBitmap(IDB_PEOPLE, RGB(255, 255, 255));
+}
+
+void CEraser::OnMove()
+{
+    const int STEP_SIZE = 4;
+    animation.OnMove();
+	
+
+    if (isMovingLeft && x >= index_x * 20)
+        x -= STEP_SIZE;
+
+    if (isMovingRight && this->GetX2() <= (index_x + 1) * 20)
+        x += STEP_SIZE;
+
+    if (isMovingUp && y >= index_y * 20)
+        y -= STEP_SIZE;
+
+    if (isMovingDown && this->GetY2() <= (index_y + 1) * 20)
+        y += STEP_SIZE;
+
+    //x:({x}), y:({y}),index_x:({index_x}),index_y({index_y})
+}
+int CEraser::GetIndexX()
+{
+    return index_x;
+}
+int CEraser::GetIndexY()
+{
+    return index_y;
+}
+void CEraser::MoveLeftIndex()
+{
+    index_x -= 1;
+}
+void CEraser::MoveRightIndex()
+{
+    index_x += 1;
+}
+void CEraser::MoveUpIndex()
+{
+    index_y -= 1;
+}
+void CEraser::MoveDownIndex()
+{
+    index_y += 1;
+}
+void CEraser::SetMovingDown(bool flag)
+{
+    isMovingDown = flag;
+}
+
+void CEraser::SetMovingLeft(bool flag)
+{
+    isMovingLeft = flag;
+}
+
+void CEraser::SetMovingRight(bool flag)
+{
+    isMovingRight = flag;
+}
+
+void CEraser::SetMovingUp(bool flag)
+{
+    isMovingUp = flag;
+}
+
+void CEraser::SetChoosen(bool flag)
+{
+	isChoosen = flag;
+}
+
+void CEraser::SetXY(int nx, int ny)
+{
+    x = nx;
+    y = ny;
+}
+void CEraser::OnShow()
+{
+	animation.SetTopLeft(x, y);
+	animation.OnShow();
+}
+bool CEraser::IsChoosen()
+{
+	return isChoosen;
+}
+void CEraser::SetRoadLine(int mouse_x, int mouse_y)
+{
+	if (roadLine.empty())
 	{
-		Initialize();
+		moving_index_x = index_x;
+		moving_index_y = index_y;
 	}
-
-	int CEraser::GetX1()
+	while (mouse_x  > moving_index_x && mouse_y <  moving_index_y)
 	{
-		return x;
+		roadLine.push_back(1);
+		moving_index_x += 1;
+		moving_index_y -= 1;
 	}
-
-	int CEraser::GetY1()
+	while (mouse_x >  moving_index_x && mouse_y >  moving_index_y)
 	{
-		return y;
+		roadLine.push_back(3);
+		moving_index_x += 1;
+		moving_index_y += 1;
 	}
-
-	int CEraser::GetX2()
+	while (mouse_x <  moving_index_x&& mouse_y >  moving_index_y)
 	{
-		return x + animation.Width();
+		roadLine.push_back(5);
+		moving_index_x -= 1;
+		moving_index_y += 1;
 	}
-
-	int CEraser::GetY2()
+	while (mouse_x < moving_index_x && mouse_y < moving_index_y)
 	{
-		return y + animation.Height();
+		roadLine.push_back(7);
+		moving_index_x -= 1;
+		moving_index_y -= 1;
 	}
-
-	void CEraser::Initialize()
+	while (mouse_x > moving_index_x)
 	{
-		const int X_POS = 280;
-		const int Y_POS = 400;
-		x = X_POS;
-		y = Y_POS;
-		isMovingLeft = isMovingRight = isMovingUp = isMovingDown = false;
+		roadLine.push_back(2);
+		moving_index_x += 1;
 	}
-
-	void CEraser::LoadBitmap()
+	while (mouse_x  < moving_index_x)
 	{
-		animation.AddBitmap(IDB_ERASER1, RGB(255, 255, 255));
-		animation.AddBitmap(IDB_ERASER2, RGB(255, 255, 255));
-		animation.AddBitmap(IDB_ERASER3, RGB(255, 255, 255));
-		animation.AddBitmap(IDB_ERASER2, RGB(255, 255, 255));
+		roadLine.push_back(6);
+		moving_index_x -= 1;
 	}
-
-	void CEraser::OnMove()
+	while (mouse_y  > moving_index_y)
 	{
-		const int STEP_SIZE = 2;
-		animation.OnMove();
-		if (isMovingLeft)
-			x -= STEP_SIZE;
-		if (isMovingRight)
-			x += STEP_SIZE;
-		if (isMovingUp)
-			y -= STEP_SIZE;
-		if (isMovingDown)
-			y += STEP_SIZE;
+		roadLine.push_back(4);
+		moving_index_y += 1;
 	}
-
-	void CEraser::SetMovingDown(bool flag)
+	while (mouse_y  < moving_index_y)
 	{
-		isMovingDown = flag;
+		roadLine.push_back(0);
+		moving_index_y -= 1;
 	}
+	
+}
+const vector<int>& CEraser::GetRoadLine()
+{
+	return roadLine;
+}
 
-	void CEraser::SetMovingLeft(bool flag)
-	{
-		isMovingLeft = flag;
-	}
-
-	void CEraser::SetMovingRight(bool flag)
-	{
-		isMovingRight = flag;
-	}
-
-	void CEraser::SetMovingUp(bool flag)
-	{
-		isMovingUp = flag;
-	}
-
-	void CEraser::SetXY(int nx, int ny)
-	{
-		x = nx; y = ny;
-	}
-
-	void CEraser::OnShow()
-	{
-		animation.SetTopLeft(x, y);
-		animation.OnShow();
-	}
 }
--- game4.10/Source/CEraser.h
+++ game4.10/Source/CEraser.h
@@ -12,21 +12,35 @@ namespace game_framework {
 		int  GetY1();					// 擦子左上角 y 座標
 		int  GetX2();					// 擦子右下角 x 座標
 		int  GetY2();					// 擦子右下角 y 座標
+		int	GetIndexX();
+		int GetIndexY();
 		void Initialize();				// 設定擦子為初始值
 		void LoadBitmap();				// 載入圖形
 		void OnMove();					// 移動擦子
 		void OnShow();					// 將擦子圖形貼到畫面
+		void MoveLeftIndex();
+		void MoveRightIndex();
+		void MoveUpIndex();
+		void MoveDownIndex();
 		void SetMovingDown(bool flag);	// 設定是否正在往下移動
 		void SetMovingLeft(bool flag);	// 設定是否正在往左移動
 		void SetMovingRight(bool flag); // 設定是否正在往右移動
 		void SetMovingUp(bool flag);	// 設定是否正在往上移動
+		void SetChoosen(bool flag);
 		void SetXY(int nx, int ny);		// 設定擦子左上角座標
+		bool IsChoosen();
+		void SetRoadLine(int mouse_x, int mouse_y);
+		const vector<int>& GetRoadLine();
 	protected:
-		CAnimation animation;		// 擦子的動畫
+		CAnimation animation,animation2;		// 擦子的動畫
 		int x, y;					// 擦子左上角座標
+		int index_x, index_y;
+		int moving_index_x, moving_index_y;
 		bool isMovingDown;			// 是否正在往下移動
 		bool isMovingLeft;			// 是否正在往左移動
 		bool isMovingRight;			// 是否正在往右移動
 		bool isMovingUp;			// 是否正在往上移動
+		bool isChoosen;
+		vector<int> roadLine;
 	};
 }
